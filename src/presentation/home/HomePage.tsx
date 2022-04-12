74cf23d84f644cfc702bf7a301c5e67be9ad61a3
Dominic <silva.d.dominic@gmail.com>
2022-04-11 20:04:11 -0400

    Refactored HomePage and NewsFeed
    - Homepage sets search params more robustly
    - Created basic implementation of NewsFeed page

--- src/presentation/home/HomePage.tsx
+++ src/presentation/home/HomePage.tsx
@@ -1,32 +1,33 @@
 import {Button, Container, Flex, Input, Text} from "@chakra-ui/react";
-import React, {FormEvent} from "react";
-import {ISearchService} from "../../application/search-service.interface";
+import React, {FormEvent, useRef} from "react";
 import {useNavigate} from "react-router-dom";
 
-export function HomePage({generalSearch}: ISearchService) {
-    const navigate = useNavigate();
+export function HomePage() {
+  const searchQuery = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
 
-    return (
-        <Container>
-            <div style={{marginTop: "30vh"}}>
-                <Text fontSize="3xl" align="center">Twitter Clone</Text>
-                <form onSubmit={search}>
-                    <Flex py={15}>
-                        <Input placeholder="Search for a user or tag" mr={2}/>
-                        <Button type="submit" colorScheme='teal' variant='outline'>Search</Button>
-                    </Flex>
-                </form>
-            </div>
-        </Container>
-    )
+  return (
+    <Container>
+      <div style={{marginTop: "30vh"}}>
+        <Text fontSize="3xl" align="center">Twitter Clone</Text>
+        <form onSubmit={search}>
+          <Flex py={15}>
+            <Input ref={searchQuery} placeholder="Search for a user or tag" mr={2}/>
+            <Button type="submit" colorScheme='teal' variant='outline'>Search</Button>
+          </Flex>
+        </form>
+      </div>
+    </Container>
+  )
 
-    function search(event: FormEvent<HTMLFormElement>) {
-        event.preventDefault();
-        generalSearch()
-            .then(res => {
-                console.log(res);
-                navigate("/search");
-            })
-            .catch(err => console.log(err));
+  function search(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const query = searchQuery.current?.value;
+    if (query) {
+      navigate({
+        pathname: "search",
+        search: `q=${query}`,
+      });
     }
+  }
 }
--- src/presentation/search/NewsFeed.tsx
+++ src/presentation/search/NewsFeed.tsx
@@ -1,11 +1,35 @@
-import {Container, Flex} from '@chakra-ui/react';
+import { Container, Flex, Text, ListItem, UnorderedList } from '@chakra-ui/react';
+import { useLocation } from "react-router";
+import { useEffect, useState } from "react";
+import { ISearchService } from "../../application/search-service.interface";
+import { Post } from '../../application/post.interface';
 
-export function NewsFeed() {
-    return (
-        <Container>
-            <Flex>
-                Search page
-            </Flex>
-        </Container>
-    )
+
+export function NewsFeed({generalSearch}: ISearchService) {
+  const {search} = useLocation();
+  const [feed, setFeed] = useState<Post[]>([]);
+
+  useEffect(() => {
+    // Fetch Posts
+    generalSearch(search)
+      .then((posts: Post[]) => {
+        setFeed(posts)
+      })
+      .catch(err => {
+        console.log(err)
+      });
+  }, []);
+
+  return (
+    <Container>
+      <Text fontSize="xl" align="center">News Feed</Text>
+      <Flex>
+        <UnorderedList>
+          {feed.map(item => {
+            return <ListItem key={item.id}>{item.title}</ListItem>
+          })}
+        </UnorderedList>
+      </Flex>
+    </Container>
+  );
 }
